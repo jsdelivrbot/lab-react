6675015f05841ceea292a05947d03ac0c04792b7
문경태(kyungtea Moon) <demoon2016@ncsoft.com>
2017-11-30 09:38:10 +0900

    .

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyBVOC5P9K21oCWp1plLb8KI_Hh6H-3ZLUM';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+            this.setState({videos});
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
