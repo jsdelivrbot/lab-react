d80413ee7238dfc6484711dc4d20dd7d4fede4f3
문경태(kyungtea Moon) <demoon2016@ncsoft.com>
2017-12-01 10:43:26 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -12,20 +13,39 @@ class App extends Component {
         super(props);
 
         this.state = {
-            videos: []
+            videos: [],
+            selectedVideo: null,
         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
-            this.setState({videos});
+        this.videoSearch('슈퍼윙스');
+    }
+
+    videoSearch(term){
+        YTSearch({key: API_KEY, term: term}, (videos) =>{
+            this.setState({
+                videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render(){
+
+        const videoSearch = _.debounce((term) =>{
+            console.log(term);
+            this.videoSearch(term)
+        }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch} />
+
+                <VideoDetail video={this.state.selectedVideo} />
+
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
