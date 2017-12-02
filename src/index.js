5db9333b326518e5771d13fee66faa26c960e627
kyungtae Moon <demoon84@naver.com>
2017-12-02 16:36:55 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,54 +1,57 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyBVOC5P9K21oCWp1plLb8KI_Hh6H-3ZLUM';
+const API_KEY = 'AIzaSyC97dT2fXE7rgTzbY5xkHA74xpKfzeXjvU';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            videos: [],
-            selectedVideo: null,
-        };
-
-        this.videoSearch('슈퍼윙스');
-    }
-
-    videoSearch(term){
-        YTSearch({key: API_KEY, term: term}, (videos) =>{
-            this.setState({
-                videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
-
-    render(){
-
-        const videoSearch = _.debounce((term) =>{
-            console.log(term);
-            this.videoSearch(term)
-        }, 300);
-
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch} />
-
-                <VideoDetail video={this.state.selectedVideo} />
-
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                    videos={this.state.videos}
-                />
-            </div>
-        );
-    }
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			videos: [],
+			selectedVideo: null,
+		};
+		
+		this.videoSearch('슈퍼윙스');
+	}
+	
+	videoSearch(term){
+		const max = 50;
+		
+		$.ajax({
+			url: `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${term}&part=snippet&type=video&maxResults=${max}`,
+			success: (videos) =>{
+				this.setState({
+					videos: videos.items,
+					selectedVideo: videos.items[0]
+				});
+			}
+		});
+	}
+	
+	render(){
+		const videoSearch = _.debounce((term) =>{
+			console.log(term);
+			this.videoSearch(term)
+		}, 300);
+		
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch}/>
+				
+				<VideoDetail video={this.state.selectedVideo}/>
+				
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos}
+				/>
+			</div>
+		);
+	}
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
