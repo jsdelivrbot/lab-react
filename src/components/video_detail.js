94dd6a2da4ea35fac04a7c113c88a675426ff045
kyungtae Moon <demoon84@naver.com>
2017-12-25 10:48:35 +0900

    .

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const VideoDetail = ({video}) =>{
-    if(!video) {
-        return <div>Loading</div>;
-    }
-
-    const videoId = video.id.videoId;
-    const url = `https://www.youtube.com/embed/${videoId}`;
-
-    return (
-        <div className='video-detail col-md-8'>
-            <div className='embed-responsive embed-responsive-16by9'>
-                <iframe className='embed-responsive-item' src={url}></iframe>
-            </div>
-
-            <div className='details'>
-                <div>{video.snippet.title}</div>
-                <div>{video.snippet.description}</div>
-            </div>
-        </div>
-    );
+	if(!video){
+		return <div>Loading</div>;
+	}
+	
+	const videoId = video.id.videoId;
+	const url = `https://www.youtube.com/embed/${videoId}?modestbranding=0&rel=0&showinfo=0&playsinline=1`;
+	
+	return (
+		<div className='video-detail col-md-8'>
+			<div className='embed-responsive embed-responsive-16by9'>
+				<iframe className='embed-responsive-item' src={url} allowFullScreen></iframe>
+			</div>
+			
+			<div className='details'>
+				<div>{video.snippet.title}</div>
+				<div>{video.snippet.description}</div>
+			</div>
+		</div>
+	);
 };
 
 export default VideoDetail;
